c8f6a53cafdc213c671da3c3acc2d43abae593f6
Sophia Kim <mail@fifikim.com>
2022-03-23 16:44:52 -0400

    add demo controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  # root route
+  root 'demo#index'
+
+  # simple route
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/contact'
+  get 'demo/about'
+
+  # default route
+  # get ':controller(/:action(/:id))'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
